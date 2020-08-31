fa8fc6fda9593ebb559cd42e3226a4d8ff0532d2
asamitakahashi <1105asamit@i.softbank.jp>
2020-08-31 22:01:38 +0900

    修正

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import RealmSwift  //　←追加
 import UserNotifications
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class ViewController: UIViewController, UITableViewDelegate, UISearchBarDelegate, UITableViewDataSource{
     @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
     
     // Realmインスタンスを取得する
     let realm = try! Realm()  //追加
@@ -25,19 +26,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
     // 追加
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        SearchBar.delegate = self
+        
     }
     
     //　入力画面から戻ってきた時に　Table view を更新させる
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
         tableView.reloadData()
-
+        
     }
     
     // データの数（＝セルの数）を返すメソッド
@@ -53,13 +55,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         //　Cellに値を設定する　-------　ここから　---
         let task = taskArray[indexPath.row]
-        cell.textLabel?.text = task.title
+        cell.textLabel?.text = task.title + "[" + task.category + "]"
+        print(task)
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm"
         
         let dateString:String = formatter.string(from: task.date)
         cell.detailTextLabel?.text = dateString
+        
+        
+        
         // --- ここまで追加　---
         
         return cell
@@ -91,35 +97,51 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 self.realm.delete(task)
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
-    //　未通知のローカル通知一覧をログ出力
-    center.getPendingNotificationRequests{ (requests: [UNNotificationRequest])
-    in
-        for request in requests{
-    print("/---------------")
-    print(request)
-    print("---------------------/")
+            //　未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests{ (requests: [UNNotificationRequest])
+                in
+                for request in requests{
+                    print("/---------------")
+                    print(request)
+                    print("---------------------/")
+                }
+            }
+        } // ーーーーーーーーー　ここまで変更　ーーーーーーーーーーーー
     }
+    
+    
+    // Query using an NSPredicate
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let predicate = NSPredicate(format: "category = %@",searchText)
+        taskArray = realm.objects(Task.self).filter(predicate)
+        tableView.reloadData()
+        print(searchText)
+        if searchText.isEmpty {
+            taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+            tableView.reloadData()
+        }
     }
-} // ーーーーーーーーー　ここまで変更　ーーーーーーーーーーーー
-}
     
-// segue で画面遷移するときに呼ばれる
-override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-    let inputViewController:InputViewController = segue.destination
-        as! InputViewController
-    
-    if segue.identifier == "cellSegue"{
-        let indexPath = self.tableView.indexPathForSelectedRow
-        inputViewController.task = taskArray[indexPath!.row]
-    }else{
-        let task = Task()
+    // segue で画面遷移するときに呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination
+            as! InputViewController
         
-        let allTasks = realm.objects(Task.self)
-        if allTasks.count != 0 {
-            task.id = allTasks.max(ofProperty: "id")! + 1
+        if segue.identifier == "cellSegue"{
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
         }
         
-        inputViewController.task = task
+        
+        
     }
-}
 }
